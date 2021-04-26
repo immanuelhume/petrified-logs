eca9da0fdd35029920080afe9d9aa36fc9c6ad29
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-04-27 00:16:57 +0800

    added 2channel emojis

--- petrified/emojis.py
+++ petrified/emojis.py
@@ -1,3 +1,60 @@
 # emojis uwu
 class EMOJIS:
+    # neutral
+    kowtow = 'm(_ _)m'
+    snubbed = '(´･ω･`)'
+    salute = '(｀-´)>'
+    surprised = '（　ﾟДﾟ）'
+    very_surprised = 'Σ(ﾟДﾟ)'
+    meh = "┐('～`；)┌"
+    mehh = '(ﾟ⊿ﾟ)'
+    indifferent = '（　´_ゝ`）'
+    smoking = '(´ー`)y-~~'
+    thinking = '（´-`）.｡oO( ... )'
+    whispers = '( ´д)ﾋｿ(´Д｀)ﾋｿ(Д｀)'
+    come = 'щ(ﾟДﾟщ) (屮ﾟДﾟ)屮'
+    concerned = '（・Ａ・）'
+    disbelief = 'エェェ(´д｀)ェェエ'
+    snorlax = '(￣ー￣)'
+    ignore = '(ΘεΘ;)'
+    running = 'ε=ε=ε=┌(;*´Д`)ﾉ'
+    point_right = '(☞ﾟヮﾟ)☞'
+    point_left = '☜(ﾟヮﾟ☜) '
+    angel = '☜(⌒▽⌒)☞'
+
+    # good vibes
+    good = '（・∀・）'
+    peaceful = 'ヽ(´ー｀)ﾉ'
+    carefree = '（´∀｀）'
     lenny = '( ͡° ͜ʖ ͡°)'
+    perky = '(`･ω･´)'
+    upbeat = '( ﾟヮﾟ)'
+    bun = '⊂二二二（＾ω＾）二⊃'
+    cheek = '( ´∀｀)σ)∀`)'
+    amazed = '( ﾟдﾟ)'
+    cheers = '（ ^_^）o自自o（^_^ ）'
+    aha = 'm9(・∀・)'
+    friendly = 'ヽ(´ー`)人(´∇｀)人(`Д´)ノ'
+    money = '（･∀･)つ⑩'
+    goofy = '(ﾟ∀ﾟ)'
+    dance = '♪┏(・o･)┛♪┗ ( ･o･) ┓'
+    happy = 'ヽ(´▽`)/'
+
+    # sad vibes
+    sad = '(´；ω；`)'
+    spook = '(((( ；ﾟДﾟ)))'
+    lonely = "('A`)"
+    depressed = '（ ´,_ゝ`)'
+    upset = '（ つ Д ｀）'
+    despair = '＿|￣|○'
+
+    # chaotic vibes
+    irritiable = 'ヽ(`Д´)ﾉ'
+    angry = '(＃ﾟДﾟ)'
+    furious = 'ヽ(ｏ`皿′ｏ)ﾉ'
+    yelling = '（ ´Д｀）'
+    shocked = 'Σ(゜д゜;)'
+    shooked = "(l'o'l)"
+    impatient = '(ﾟДﾟ;≡;ﾟДﾟ)'
+    grimace = '(╬ ಠ益ಠ)'
+    shouting = '(≧ロ≦)'
